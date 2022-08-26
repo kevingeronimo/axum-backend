abcbd9e1e2739a6bd386e2913a72b06c43eb8e57
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-08-26 15:14:40 +0000

    Handle errors for user_controller and main

--- src/controllers/user_controller.rs
+++ src/controllers/user_controller.rs
@@ -1,10 +1,26 @@
-use crate::services::user_service;
-use axum::{response::IntoResponse, Extension, http::StatusCode, Json};
+use crate::{error, services::user_service};
+use error_stack::{IntoReport, ResultExt};
+use axum::{http::StatusCode, Extension, Json, response::IntoResponse};
 use sqlx::PgPool;
+use tracing::{Level, event, instrument};
 
-pub async fn get_all_users(Extension(pool): Extension<PgPool>) -> impl IntoResponse {
+#[instrument]
+pub async fn get_all_users(
+    Extension(pool): Extension<PgPool>,
+) -> Result<impl IntoResponse, error::Error> {
     let us = user_service::UserService::new(&pool);
-    let users = us.get_all_users().await.unwrap();
+    let users = us
+        .get_all_users()
+        .await
+        .report()
+        .attach_printable("Could not retrieve all users")
+        .change_context(error::Error::InternalServerError);
 
-    (StatusCode::OK, Json(users))
+        match users {
+            Ok(users) => Ok((StatusCode::OK, Json(users))),
+            Err(e) => {
+                event!(Level::ERROR, "{e:?}");
+                Err(*e.current_context())
+            }
+        }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,40 @@
-use hearthstone_backend::controllers::user_controller;
-use axum::{
-    routing::get,
-    Router, Extension,
+use axum::{routing::get, Extension, Router};
+use error_stack::{IntoReport, ResultExt};
+use hearthstone_backend::{
+    controllers::user_controller,
+    error
 };
 use sqlx::postgres::PgPoolOptions;
+use tower_http::trace::TraceLayer;
+use tracing::{Level, event, instrument};
 
+#[instrument]
 #[tokio::main]
-async fn main() -> Result<(), sqlx::Error> {
+async fn main() {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var(
+            "RUST_LOG",
+            "hearthstone_backend=debug,tower_http=debug",
+        )
+    }
+    tracing_subscriber::fmt::init();
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect("postgres://user:pass@postgres_container/postgres").await?;
+        .connect("postgres://user:pass@postgres_container/postgres")
+        .await
+        .report()
+        .attach_printable("Unable to connect to postgres database")
+        .map_err(|e| {
+            event!(Level::ERROR, "{e:?}");
+            std::process::exit(101)
+        });
 
     let app = Router::new()
-    .route("/", get(|| async { "Hello, World!" }))
-    .route("/users", get(user_controller::get_all_users))
-    .layer(Extension(pool));
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/users", get(user_controller::get_all_users))
+        .layer(Extension(pool))
+        .layer(TraceLayer::new_for_http());
 
     // run it with hyper on localhost:3000
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
@@ -22,5 +42,4 @@ async fn main() -> Result<(), sqlx::Error> {
         .await
         .unwrap();
 
-    Ok(())
 }
